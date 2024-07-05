0f3f7d1ac41958b5a170f357a836c65affdf5adc
AndreasDelabie <andreas.delabie@gmail.com>
2024-07-05 09:10:51 +0200

    first commit

---
+++ playShowreel.js
@@ -0,0 +1,6 @@
+async function playShowreel() {
+    function playShowreelVideo() {
+        document.getElementById("showreel").src = "https://www.youtube-nocookie.com/embed/SRDN3N5zgBY?vq=hd1080&autoplay=1&mute=1&modestbranding=1&rel=0&controls=1"
+    }
+    await playShowreelVideo();
+}
