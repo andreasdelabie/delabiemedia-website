9f0d3ecb4cc9078ebfd1765dec1b46786e34eb47
AndreasDelabie <andreas.delabie@gmail.com>
2024-07-10 10:26:54 +0200

    Animation improved for slow page loading on mobile

--- animation-home.js
+++ animation-home.js
@@ -3,5 +3,5 @@ gsap.registerPlugin(ScrollTrigger);
 
 const timeline = gsap.timeline()
 timeline
-    .from('.logo', {duration: 1, delay: '.5', opacity: 0, ease: 'power1.in'})
-    .from('header', {duration: 1, delay: '.5', y: '-100%', ease: 'power3.out'})
+    .to('.logo', {duration: 1, delay: '.5', opacity: 1, ease: 'power1.in'})
+    .to('header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
