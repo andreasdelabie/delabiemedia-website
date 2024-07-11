a8c789b7e8c2099237a60341bf5929dd43648010
AndreasDelabie <andreas.delabie@gmail.com>
2024-07-11 18:39:05 +0200

    Breakdown sectie op homepagina

--- test/animation-test.js
+++ test/animation-test.js
@@ -1,11 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
-const timeline = gsap.timeline()
-timeline
-    .to('.logo', {duration: 1, delay: '.5', opacity: 1, ease: 'power1.in'})
-    .to('header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
-
 gsap.to('.after1', {
     scrollTrigger: {
         trigger: '.breakdown1',
