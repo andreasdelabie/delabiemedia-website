8d85528e9e966806821349cfc153456d36632927
AndreasDelabie <andreas.delabie@gmail.com>
2024-07-15 10:17:48 +0200

    Faster loading homepage

--- animation-home.js
+++ animation-home.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const timeline = gsap.timeline()
 timeline
-    .to('.logo', {duration: 1, delay: '.5', opacity: 1, ease: 'power1.in'})
+    .to('.logo', {duration: 1, opacity: 1, ease: 'power1.in'})
     .to('header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
 
 gsap.to('.after1', {
