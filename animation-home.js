2d623e4a95249d08678cb8c4106e42da02f8696b
AndreasDelabie <andreas.delabie@gmail.com>
2024-07-18 21:56:04 +0200

    Fixed bug where only 1 breakdown description showed, general JavaScript cleanup & added comments, funny 'Delabie Media' ascii art in index.html

--- animation-home.js
+++ animation-home.js
@@ -1,12 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
+// Homepage logo & header loading animation
 const timeline = gsap.timeline()
 timeline
     .to('.logo', {duration: 1, opacity: 1, ease: 'power1.in'})
     .to('header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
 
 
+// Breakdown scroll animations
 gsap.to('.after1', {
     scrollTrigger: {
         trigger: '.breakdown1',
@@ -28,6 +30,8 @@ gsap.to('.after2', {
 
 // Fixing stupid element with absolute positioning that positions wrong on load for a second...
 window.onload = function() {
-    var element = document.getElementById('breakdown-description');
-    element.style.display = 'block';
+    var element = document.querySelectorAll('#breakdown-description');
+    for (var i = 0, max = element.length; i < max; i++) {
+        element[i].style.display = 'block';
+    }
 }
--- cookies/cookies.js
+++ cookies/cookies.js
@@ -1,13 +1,17 @@
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
     let expires = "expires="+ d.toUTCString();
+
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
+
     for(let i = 0; i <ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) == ' ') {
@@ -17,19 +21,23 @@ function getCookie(cname) {
         return c.substring(name.length, c.length);
       }
     }
+
     return "";
 }
 
 
+// Check if cookies have been accepted before
 var acceptedCookies = getCookie('acceptedCookies')
 const cookiebanner = document.getElementById('cookiebanner')
 
 if(acceptedCookies == 'true'){
-    cookiebanner.style.display = 'none'
+    cookiebanner.style.display = 'none' // Hide cookie banner if yes
 }else {
-    cookiebanner.style.display = 'block'
+    cookiebanner.style.display = 'block' // Show cookie banner if no
 }
 
+
+// Add 'acceptedCookies' cookie
 function acceptCookies(){
     setCookie('acceptedCookies', 'true', 30)
     cookiebanner.style.display = 'none'
