d60498fbda7406dbbe0d350734c639fcdf5c3c6d
AndreasDelabie <andreas.delabie@gmail.com>
2024-08-21 22:04:38 +0200

    Updated header references

--- js/animation-home.js
+++ js/animation-home.js
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 const timeline = gsap.timeline()
 timeline
     .to('.logo', {duration: 1, opacity: 1, ease: 'power1.in'})
-    .to('main-header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
+    .to('header', {duration: 1, delay: '.5', y: '0%', ease: 'power3.out'})
 
 
 // Breakdown scroll animations
